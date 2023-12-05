dbd0413862f5c45ebe33e40b71e5994c6b1fdc4d
JuanJoven01 <juanjov95@hotmail.com>
2023-12-04 19:22:43 -0500

    ssl

--- config/database.js
+++ config/database.js
@@ -6,20 +6,23 @@ module.exports ={
     "password": process.env.POSTGRES_PW,
     "database": process.env.POSTGRES_DB,
     "host": process.env.POSTGRES_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "ssl": true
   },
   "test": {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PW,
     "database": process.env.POSTGRES_DB,
     "host": process.env.POSTGRES_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "ssl": true
   },
   "production": {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PW,
     "database": process.env.POSTGRES_DB,
     "host": process.env.POSTGRES_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "ssl": true
   }
 }
