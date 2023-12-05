3c8f9104e9d82237acedc2d8aba8d5cec575b0dd
JuanJoven01 <juanjov95@hotmail.com>
2023-12-04 19:41:51 -0500

    database.js changed

--- config/database.js
+++ config/database.js
@@ -1,25 +1,43 @@
 require('dotenv').config()
 
 module.exports ={
-  "development": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PW,
-    "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
-    "dialect": "postgres",
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PW,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
-  "test": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PW,
-    "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
-    "dialect": "postgres",
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PW,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
-  "production": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PW,
-    "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
-    "dialect": "postgres",
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PW,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
